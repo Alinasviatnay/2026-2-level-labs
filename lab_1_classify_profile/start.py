d04e592374bc7e685dad6472681a52a84559bc33
Alina <alina.sviatnay@mail.ru>
2026-09-20 17:48:05 +0300

    добавила вызовы функций для детекции неизвестного языка

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -5,31 +5,17 @@ Language detection starter.
 # pylint: disable=unused-variable, duplicate-code
 
 
-from main import tokenize
-from main import remove_stop_words
-from main import calculate_frequencies
-from main import  get_top_n_words
+from main import (tokenize,
+                  remove_stop_words,
+                  calculate_frequencies,
+                  get_top_n_words,
+                  create_language_profile,
+                  compare_profiles_by_top_n,
+                  detect_language_by_top_n
+)
 
 
-# Токенизируем тексты
-#de_tokens = tokenize(de.txt)
-#unknown_tokens = tokenize(unknown.txt)
-#en_tokens = tokenize(en.txt)
 
-    # Удаляем стоп-слова
-#de_tokens = remove_stop_words(de.txt, stopwords)
-#unknown_tokens = remove_stop_words(unknown.txt, stopwords)
-#en_tokens = remove_stop_words(en.txt, stopwords)
-
-    # Считаем частоты
-#de_freq = calculate_frequencies(de.txt)
-#unknown_freq = calculate_frequencies(unknown.txt)
-#en_freq = calculate_frequencies(en.txt)
-
-    # Получаем топ-10 слов
-#de_top = get_top_n_words(de_freq, 10)
-#unknown_top = get_top_n_words(unknown_freq, 10)
-#en_top = get_top_n_words(en_freq, 10)
 
 
 def main() -> None:
@@ -46,19 +32,22 @@ def main() -> None:
         en_text = file.read()
 
     de_tokens = tokenize(de_text)
-    unknown_tokens = tokenize(unknown_text)
     en_tokens = tokenize(en_text)
+    unknown_tokens = tokenize(unknown_text)
 
     de_tokens = remove_stop_words(de_tokens, stopwords)
-    unknown_tokens = remove_stop_words(unknown_tokens, stopwords)
     en_tokens = remove_stop_words(en_tokens, stopwords)
+    unknown_tokens = remove_stop_words(unknown_tokens, stopwords)
 
-    de_freq = calculate_frequencies(de_tokens)
-    unknown_freq = calculate_frequencies(unknown_tokens)
-    en_freq = calculate_frequencies(en_tokens)
+    de_profile = create_language_profile("de", de_text, stopwords)
+    en_profile = create_language_profile("en", en_text, stopwords)
+    unknown_profile = create_language_profile("unknown", unknown_text, stopwords)
 
-    result = None
+    result = detect_language_by_top_n(unknown_profile, en_profile, de_profile, 15)
+    print(result)
     assert result, "Detection result is None"
+
+
 
 
 if __name__ == "__main__":
