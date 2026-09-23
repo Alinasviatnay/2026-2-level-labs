9a6d94ebbaca16233b865e43bb97ba41bd5d29fc
Alina <alina.sviatnay@mail.ru>
2026-09-24 00:11:01 +0300

    добавила вызовы функций

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -5,18 +5,15 @@ Language detection starter.
 # pylint: disable=unused-variable, duplicate-code
 
 
-from main import (tokenize,
-                  remove_stop_words,
-                  calculate_frequencies,
-                  get_top_n_words,
-                  create_language_profile,
+from main import (calculate_frequencies,
                   compare_profiles_by_top_n,
-                  detect_language_by_top_n
+                  create_language_profile,
+                  detect_language_by_mse,
+                  detect_language_by_top_n,
+                  get_top_n_words,
+                  remove_stop_words,
+                  tokenize,
 )
-
-
-
-
 
 def main() -> None:
     """
@@ -39,11 +36,28 @@ def main() -> None:
     en_tokens = remove_stop_words(en_tokens, stopwords)
     unknown_tokens = remove_stop_words(unknown_tokens, stopwords)
 
+    de_freq = calculate_frequencies(de_tokens)
+    en_freq = calculate_frequencies(en_tokens)
+    unknown_freq = calculate_frequencies(unknown_tokens)
+
+    top_n_words = get_top_n_words(de_freq, 7)
+
+    print(f"Топ-7 слов немецкого текста: {top_n_words}")
+
     de_profile = create_language_profile("de", de_text, stopwords)
     en_profile = create_language_profile("en", en_text, stopwords)
     unknown_profile = create_language_profile("unknown", unknown_text, stopwords)
 
-    result = detect_language_by_top_n(unknown_profile, en_profile, de_profile, 15)
+    detected_language_top_n = detect_language_by_top_n(unknown_profile,
+                                                       en_profile, de_profile, 15)
+
+    print(f"Определённый язык (по топ-15 словам): {detected_language_top_n}")
+
+    result = detect_language_by_mse(
+        unknown_profile=unknown_profile,
+        profile_1=en_profile,
+        profile_2=de_profile
+    )
     print(result)
     assert result, "Detection result is None"
 
