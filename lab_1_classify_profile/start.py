fa5fdd9cadc4cf495dc379c496b201dbb2ee4b9f
Alina <alina.sviatnay@mail.ru>
2026-09-25 01:28:03 +0300

    work with CI

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -56,9 +56,9 @@ def main() -> None:
     unknown_profile = create_language_profile("unknown", unknown_text, stopwords)
 
     if de_profile is None or \
-        en_profile is None or \
-        unknown_profile is None:
-            return
+    en_profile is None or \
+    unknown_profile is None:
+        return
 
     detected_language_top_n = detect_language_by_top_n(unknown_profile,
                                                        en_profile, de_profile, 15)
